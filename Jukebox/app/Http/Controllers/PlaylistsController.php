d9f666b36f9780424bb7d6f0dc897c778bf7d24d
MasterDragon1998 <d.raangs@gmail.com>
2018-05-20 00:01:03 +0200

    Personalized playlists

--- Jukebox/app/Http/Controllers/PlaylistsController.php
+++ Jukebox/app/Http/Controllers/PlaylistsController.php
@@ -23,7 +23,7 @@ class PlaylistsController extends Controller
      */
     public function index()
     {
-        return view('playlists.index', ['playlists' => Playlist::all()]);
+        return view('playlists.index', ['playlists' => auth()->user()->playlists]);
     }
 
     /**
